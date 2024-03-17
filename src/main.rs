f6f531bc2ff9e5e22740a35597f717d702d5f00f
Luka Markovic <lukamarkovic@Lukas-MacBook-Pro.local>
2024-03-17 10:02:09 +0100

    Open image as bytes and print it.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::env;
+use std::fs;
+
+fn print_bytes(bytes : &[u8]) {
+    let n = bytes.len();
+    for i in 0..n {
+        if i % 8 == 0 {
+            print!("\n{:#x}, ", bytes[i]);
+        }
+        else {
+            print!("{:#x}, ", bytes[i]);
+        }
+    }
+    println!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+    let argc = args.len();
+    if argc != 2 {
+        panic!("Usage: rcomp image");
+    }
+
+    let image_name = &args[1];
+    let image_bytes = fs::read(image_name).expect("Was not able to read the file.");
+    
+    if image_name.ends_with(".png") {
+        println!("This is a png file!");
+        println!("Signature is: ");
+        print_bytes(&image_bytes[0..8]);
+    }
+
+    else {
+        println!("Unknown file!");
+        println!("Printing bytes...");
+        print_bytes(&image_bytes);
+    }
+
 }
