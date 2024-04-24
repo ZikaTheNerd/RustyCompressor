d51135ab18494055f25536b01f338ca3b60aeef6
UrosSvilar <uros.svilar@gmail.com>
2024-04-24 04:36:27 +0200

    read header and appn section

---
+++ src/constants.rs
@@ -0,0 +1,22 @@
+
+pub mod constants {
+    pub const SOI: u8 = 0xD8;
+
+    //APPN macros
+    pub const APP0: u8 = 0xE0;
+    pub const APP1: u8 = 0xE1;
+    pub const APP2: u8 = 0xE2;
+    pub const APP3: u8 = 0xE3;
+    pub const APP4: u8 = 0xE4;
+    pub const APP5: u8 = 0xE5;
+    pub const APP6: u8 = 0xE6;
+    pub const APP7: u8 = 0xE7;
+    pub const APP8: u8 = 0xE8;
+    pub const APP9: u8 = 0xE9;
+    pub const APP10: u8 = 0xEA;
+    pub const APP11: u8 = 0xEB;
+    pub const APP12: u8 = 0xEC;
+    pub const APP13: u8 = 0xED;
+    pub const APP14: u8 = 0xEE;
+    pub const APP15: u8 = 0xEF;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,73 @@
 use std::env;
 use std::fs;
 
-fn print_bytes(bytes : &[u8]) {
-    let n = bytes.len();
-    for i in 0..n {
-        if i % 8 == 0 {
-            print!("\n{:#x}, ", bytes[i]);
-        }
-        else {
-            print!("{:#x}, ", bytes[i]);
-        }
+mod constants;
+use constants::constants::*;
+
+fn read_header(bytes: &Vec<u8>) 
+{
+    if bytes.len()<2
+    {
+        panic!("Header error!\n");
     }
-    println!();
+    let first = bytes[0];
+    let second = bytes[1];
+    if first != 0xFF || second != SOI
+    {
+        panic!("Header error!\n");
+    }
+}
+
+fn read_appn(bytes: &Vec<u8>) -> usize
+{
+    let mut iterator = 2;
+    let mut first: u8 = 0;
+    let mut second: u8 = 0;
+    loop 
+    {
+        if bytes.len() < iterator+4
+        {
+            panic!("APPN error!\n");
+        }
+        first = bytes[iterator];
+        iterator+=1;
+        second = bytes[iterator];
+        iterator+=1;
+        if first != 0xFF || second<APP0 || second>APP15
+        {
+            iterator-=2;
+            break;
+        }
+        first = bytes[iterator];
+        iterator+=1;
+        second = bytes[iterator];
+        iterator+=1;
+        let length= (first as u16) << 8 | second as u16;
+        iterator+=(length as usize) - 2;
+    }
+    iterator
 }
 
 fn main() {
     let args : Vec<String> = env::args().collect();
     let argc = args.len();
     if argc != 2 {
-        panic!("Usage: rcomp image");
+        panic!("Usage: rcomp image\n");
     }
 
     let image_name = &args[1];
-    let image_bytes = fs::read(image_name).expect("Was not able to read the file.");
+    let bytes = fs::read(image_name).expect("Was not able to read the file.\n");
     
-    if image_name.ends_with(".png") {
-        println!("This is a png file!");
-        println!("Signature is: ");
-        print_bytes(&image_bytes[0..8]);
-    }
+    read_header(&bytes);
 
-    else {
-        println!("Unknown file!");
-        println!("Printing bytes...");
-        print_bytes(&image_bytes);
-    }
+    println!("Succesfully read JPEG header!\n");
 
+    let i = read_appn(&bytes);
+
+    //citanje se nastavlja od mesta i (pocev od njega)
+
+    println!("Succesfully read APPN section!\n");
+
+    println!("Next bytes are {:x} {:x} and i is {}\n", bytes[i],bytes[i+1],i);
+    
 }
